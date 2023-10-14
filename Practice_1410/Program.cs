a350c1d1d046b46b0bc12c1099255a7b871ed6ac
Илья <vkontakte13011997@gmail.com>
2023-10-14 14:03:44 +0300

    add foobar task

---
+++ Practice_1410/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice_1410 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FooBar(15));
+            Console.WriteLine(FooBar(5));
+            Console.WriteLine(FooBar(3));
+            Console.WriteLine(FooBar(17));
+        }
+
+        static string FooBar(int num)
+        {
+            if (num % 3 == 0)
+            {
+                return num % 5 == 0 ? "foobar" : "foo";
+            }
+            
+            if (num % 5 == 0)
+            {
+                return num % 3 == 0 ? "foobar" : "bar";
+            }
+
+            return "";
+        }
+    }
+}
